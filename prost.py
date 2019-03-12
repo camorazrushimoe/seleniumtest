53df2ef076b8809a4dee110c5e1e87221cc4f797
Sergey Ryabushko <biginf.com@gmail.com>
2019-03-12 16:53:26 +0300

    add file

---
+++ prost.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_check_quizpleas(self):
+        driver = self.driver
+        # driver.get("https://mozgva.com/?city_id=1#sign_in")
+        driver.get("https://mozgva.com/franchise/dashboard")
+        input = driver.find_element_by_xpath("//*[@id=\"user_email\"]")
+        input.send_keys("biginf.com@gmail.com")
+        input2 = driver.find_element_by_xpath("//*[@id=\"user_password\"]")
+        input2.send_keys("")
+        submit = driver.find_element_by_xpath("//*[@id=\"sign_in_user\"]/input[4]")
+        submit.click()
+        driver.get("https://mozgva.com/franchise/dashboard")
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
