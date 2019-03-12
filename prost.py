8da26430d3fd5f050bf4d5211cce6d484e8a1e5f
Sergey Ryabushko <biginf.com@gmail.com>
2019-03-12 16:56:34 +0300

    remove privet info

--- prost.py
+++ prost.py
@@ -10,11 +10,11 @@ class PythonOrgSearch(unittest.TestCase):
     def test_check_quizpleas(self):
         driver = self.driver
         # driver.get("https://mozgva.com/?city_id=1#sign_in")
-        driver.get("https://mozgva.com/franchise/dashboard")
+        # driver.get("https://mozgva.com/franchise/dashboard")
         input = driver.find_element_by_xpath("//*[@id=\"user_email\"]")
-        input.send_keys("biginf.com@gmail.com")
+        input.send_keys("*")
         input2 = driver.find_element_by_xpath("//*[@id=\"user_password\"]")
-        input2.send_keys("")
+        input2.send_keys("*")
         submit = driver.find_element_by_xpath("//*[@id=\"sign_in_user\"]/input[4]")
         submit.click()
         driver.get("https://mozgva.com/franchise/dashboard")
